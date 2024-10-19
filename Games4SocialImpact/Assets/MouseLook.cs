cc2aea1fbb6c9303a7b5ab4969db9e96e6fb4082
Ram Adesara <RKA22@pitt.edu>
2024-10-19 12:08:39 -0400

    Player can now look around and walk (WASD)

--- Games4SocialImpact/Assets/MouseLook.cs
+++ Games4SocialImpact/Assets/MouseLook.cs
@@ -6,11 +6,12 @@ public class MouseLook : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
     public Transform playerBody;
+    float xRotation = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -18,6 +19,12 @@ public class MouseLook : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        // This means we are rotating the camera on the x axis, remember right hand rule
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
         playerBody.Rotate(Vector3.up * mouseX);
     }
