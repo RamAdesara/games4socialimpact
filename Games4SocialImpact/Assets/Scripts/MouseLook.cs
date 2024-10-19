df53222e94b58930bfd56acba387d2dd5f6f4c44
Ram Adesara <RKA22@pitt.edu>
2024-10-19 13:50:03 -0400

    Player can now pick up objects

--- Games4SocialImpact/Assets/Scripts/MouseLook.cs
+++ Games4SocialImpact/Assets/Scripts/MouseLook.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MouseLook : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
     public Transform playerBody;
     float xRotation = 0f;
+
+    public LayerMask pickable;
+    public float shootRange = 10f;
+
+    public Transform hand;
+    GameObject pickedObject;
+    bool isPicked = false;
 
     // Start is called before the first frame update
     void Start()
@@ -27,5 +35,46 @@ public class MouseLook : MonoBehaviour
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
         playerBody.Rotate(Vector3.up * mouseX);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (isPicked) {
+                Drop();
+            } else {
+                PickUp();
+            }
+        }
+    }
+
+    void PickUp()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out hit, shootRange))
+        {
+            pickedObject = hit.transform.gameObject;
+            // If pickedObject is not null and it is pickable
+            if ( (pickedObject != null) && (((1 << pickedObject.layer) & pickable) != 0) )
+            {
+                // Pick it up
+                isPicked = true;
+                Rigidbody rb = pickedObject.GetComponent<Rigidbody>();
+                rb.isKinematic = true;
+
+                pickedObject.transform.SetParent(hand);
+                pickedObject.transform.localPosition = Vector3.zero;
+                pickedObject.transform.localRotation = Quaternion.identity;
+            }
+        }
+    }
+
+    void Drop() {
+        isPicked = false;
+
+        if (pickedObject != null)
+        {
+            pickedObject.transform.SetParent(null);
+            Rigidbody rb = pickedObject.GetComponent<Rigidbody>();
+            rb.isKinematic = false;
+        }
     }
 }
